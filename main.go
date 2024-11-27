2a49118e1762e6a1bf8206bae0b30bc8480fe671
austinwilson1296 <austinlax20@gmail.com>
2024-11-27 13:37:20 -0500

    added logic to fetch feeds by user

--- main.go
+++ main.go
@@ -42,6 +42,8 @@ func main() {
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
 	cmds.register("agg", handlerAgg)
+	cmds.register("addfeed", handlerAddFeed)
+	cmds.register("feeds", handlerFeeds)
 
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: cli <command> [args...]")
